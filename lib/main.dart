f86a103f2c31c3dd8008ec76172fb3899f8e90ff
ibrahmozdemir1 <ibrahim.ozdemir@bil.omu.edu.tr>
2021-01-08 22:56:58 +0300

    resim ekleme tamamlanacak

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:mobil_final_proje/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:mobil_final_proje/yoneticiekranıbutonları/urunEkle.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp( MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.blue),
-    home: urunEkle(),
-    debugShowCheckedModeBanner: false,
-   ),
+  runApp(
+    MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: UrunEkle(),
+      debugShowCheckedModeBanner: false,
+    ),
   );
 }
