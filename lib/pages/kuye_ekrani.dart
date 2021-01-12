bd462cd2339c4b011edbaf356a493c9736d42913
oguzcan-katar <oguzcankatar@gmail.com>
2021-01-12 13:19:30 +0200

    Kurye ekranında bazı değişiklikler yapıldı.

--- lib/pages/kuye_ekrani.dart
+++ lib/pages/kuye_ekrani.dart
@@ -14,7 +14,26 @@ class _KuryeEkraniState extends State<KuryeEkrani> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home ${widget.user.user.email}'),
+        backgroundColor: Colors.purple,
+        title: Text('Hoş Geldiniz.'),
+      ),
+      body: Form(
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(75, 50, 10, 10),
+                child: RaisedButton(
+                  // padding: EdgeInsets.fromLTRB(10, 100, 10, 100),
+                  onPressed: () {},
+                  child: Text('Tanımlanan Siparişleri Görüntüle'),
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
